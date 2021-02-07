066dc4063a5d253d9817432e9284d93e1df5b326
Walter Ramos <juramosd@gmail.com>
2021-02-07 01:17:04 -0500

    feat(1b): Crear componentes de página y delegar la gestión de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/home/home.module').then(m=>m.HomeModule)
+  },
+  {
+    path: 'course',
+    loadChildren: () => import('./modules/course/course.module').then(m=>m.CourseModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./modules/admin/admin.module').then(m=>m.AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
